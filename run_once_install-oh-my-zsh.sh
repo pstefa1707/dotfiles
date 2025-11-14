1828ec34417896184469318a197abe64f13152cb
Paras Stefanopoulos <paras@parsed.com>
2025-11-14 14:38:55 -0800

    Update run one script

--- run_once_install-oh-my-zsh.sh
+++ run_once_install-oh-my-zsh.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Install zsh if not present
+if ! command -v zsh &> /dev/null; then
+    echo "Zsh is not installed. Installing zsh..."
+    sudo apt update
+    sudo apt install zsh -y
+fi
+
+# Install Oh My Zsh if not present
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Installing Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
